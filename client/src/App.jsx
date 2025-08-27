a8a7e212803497d014e505b2f8b95befc540ba28
mann chopda <manchopda1508@gmail.com>
2025-08-27 10:25:10 +0530

    added folder structure

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from "./assets/logo.png"
+
+const App = () => {
+  return (
+    <div className=' bg-yellow-300 h-screen w-screen'>
+      <img src={logo} alt="" className='' />
+    </div>
+  )
+}
+
+export default App; 
